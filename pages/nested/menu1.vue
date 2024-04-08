9a3d264eea67c959b0ee4ffefe1d1af7f7fdcac2
kawaguchi <kawaguchihiroshi0212@gmail.com>
2024-04-08 16:28:09 +0900

    ４月８日作業内容

--- pages/nested/menu1.vue
+++ pages/nested/menu1.vue
@@ -2,25 +2,18 @@
 definePageMeta({
   title: "ユーザー新規登録",
   icon: "mdi-animation",
+  keepalive: true,
 })
-
-// interface NewUserData {
-//   name: string
-//   email: string
-//   select: number
-//   checkagree: boolean
-// }
-// const { data: newUsers } = await useFetch<{ data: NewUserData[] }>(
-//   'http://127.0.0.1:8000/department',
-// )
 
 const handleSubmit = async () => {
   const postData = {
-    name: "Value 1",
-    mail: "Value 2",
-    description: "Value 3",
+    name: name.value,
+    mail: mail.value,
+    select: select.value,
+    checkagree: checkagree.value,
   }
-  await fetchData("POST", "http://127.0.0.1:8000/newusers", postData)
+  console.log("postData:", postData)
+  await fetchData("POST", "http://127.0.0.1:8000/newuser", postData)
 }
 
 const loading = ref(false)
@@ -46,8 +39,8 @@ const fetchData = async (method: string, url: string, body: any) => {
 }
 
 const name = ref("")
-const email = ref("")
-const select = ref(null)
+const mail = ref("")
+const select = ref("選択してください")
 const checkagree = ref(false)
 const items = ["管理者", "編集者", "確認者"]
 const itemsValue = ["001", "002", "003"]
@@ -57,11 +50,13 @@ const nameRules = [
   (v: string) => !!v || "氏名は必須項目です",
   (v: string) => (v && v.length <= 10) || "氏名は10文字以内で記入してください",
 ]
-
 const emailRules = [
   (v: string) => !!v || "メールアドレスは必須項目です",
   (v: string) =>
     /.+@.+\..+/.test(v) || "メールアドレスの書式で入力してください",
+]
+const selectRules = [
+  (v: string) => !!v || v == "選択してください" || "権限は必須項目です",
 ]
 
 const form = ref(null)
@@ -78,6 +73,7 @@ const form = ref(null)
         >
       </v-row>
       <v-row class="mx-4 mb-5">
+        <v-col> {{ name }}、{{ mail }}、{{ select }}、{{ checkagree }} </v-col>
         <v-form ref="form" style="width: 500px" lazy-validation>
           <v-text-field
             v-model="name"
@@ -87,7 +83,7 @@ const form = ref(null)
             required
           />
           <v-text-field
-            v-model="email"
+            v-model="mail"
             class="mt-6"
             :rules="emailRules"
             label="メールアドレス"
@@ -99,20 +95,20 @@ const form = ref(null)
             :items="items"
             :item-title="items"
             :item-value="itemsValue"
-            :rules="[(v) => !!v || '権限は必須項目です']"
+            :rules="selectRules"
             label="権限"
             required
           />
           <v-checkbox
             v-model="checkagree"
-            :rules="[(v) => !!v || '登録を行うには同意する必要があります。']"
+            :rules="[(v) => !!v || '登録を行うには同意する必要があります']"
             label="登録に同意しますか？"
             required
           />
           <v-btn
             :disabled="!valid"
             color="success"
-            class="mr-4"
+            class="mt-6 mr-4"
             @click="handleSubmit"
           >
             登録する
--- pages/nested/menu2/menu2-1.vue
+++ pages/nested/menu2/menu2-1.vue
@@ -1,9 +1,192 @@
 <script setup lang="ts">
 definePageMeta({
-  title: 'Menu 2-1',
-  icon: 'mdi-animation',
+  title: "ログイン",
+  icon: "mdi-animation",
 })
+
+const handleSubmit = async () => {
+  const postData = {
+    loginid: loginid.value,
+    password: password.value,
+  }
+  state.value.loginid = loginid.value
+  state.value.password = password.value
+
+  localStorage.setItem(
+    "my-store",
+    '{"loginid": ' + loginid.value + ', "password": ' + password.value + "}",
+  )
+  await fetchData("POST", "http://127.0.0.1:8000/newuser", postData)
+}
+
+const loading = ref(false)
+const error = ref(null)
+const data = ref(null)
+const fetchData = async (method: string, url: string, body: any) => {
+  loading.value = true
+  try {
+    const response = await fetch(url, {
+      method: method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    })
+    const responseData = await response.json()
+    data.value = responseData
+  } catch (err: unknown | any) {
+    error.value = err.message
+  } finally {
+    loading.value = false
+  }
+}
+
+const loginid = ref("")
+const password = ref("")
+const valid = ref(true)
+const state = ref({
+  loginid: "",
+  password: "",
+})
+
+import { formatDate, useRefHistory, useStorage } from "@vueuse/core"
+
+function format(ts: number) {
+  return formatDate(new Date(ts), "YYYY-MM-DD HH:mm:ss")
+}
+
+const { history, undo, redo, canUndo, canRedo } = useRefHistory(state, {
+  deep: true,
+})
+
+const stateData = ref<string>("")
+
+onMounted(() => {
+  stateData.value = localStorage.getItem("my-store") || "データがありません"
+})
+
+// delete data from storage
+// state.value = null
+
+const loginidRules = [
+  (v: string) => !!v || "氏名は必須項目です",
+  (v: string) => (v && v.length <= 10) || "氏名は10文字以内で記入してください",
+]
+const passwordRules = [
+  (v: string) => !!v || "メールアドレスは必須項目です",
+  (v: string) =>
+    /.+@.+\..+/.test(v) || "メールアドレスの書式で入力してください",
+]
+
+const form = ref(null)
+
+// ナビゲーションガード
+const isModalOpen = ref(false)
+
+onBeforeRouteLeave((to, from, next) => {
+  const answer = window.confirm("ページを移動しますか？")
+  if (answer) {
+    next() // ページを移動する
+  } else {
+    next(false) // ページ移動をキャンセルする
+  }
+})
+
+// onBeforeRouteLeave((to, from, next) => {
+//   isModalOpen.value = true
+//   const closeModal = () => {
+//     next(false) // ページ移動をキャンセルする
+//     isModalOpen.value = false
+//   }
+//   const confirmMove = () => {
+//     isModalOpen.value = false
+//     next() // ページを移動する
+//   }
+// })
 </script>
 <template>
-  <v-container fluid> empty page </v-container>
+  <v-container fluid>
+    <v-card>
+      <v-row class="pa-4">
+        <v-col> ログイン画面 </v-col>
+        <v-col>
+          <p v-if="loading">Loading...</p>
+          <p v-if="error">{{ error }}</p>
+          <p v-if="data">Response: {{ data }}</p></v-col
+        >
+      </v-row>
+      <v-row class="mx-4 mb-5">
+        <v-form ref="form" style="width: 500px" lazy-validation>
+          <v-text-field
+            v-model="loginid"
+            :counter="10"
+            :rules="loginidRules"
+            label="ログインID"
+            required
+          />
+          <v-text-field
+            v-model="password"
+            class="mt-6"
+            :rules="passwordRules"
+            label="パスワード"
+            required
+          />
+          <v-btn
+            :disabled="!valid"
+            color="success"
+            class="mt-6 mr-4"
+            @click="handleSubmit"
+          >
+            ログインする
+          </v-btn>
+        </v-form>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-btn :disabled="!canUndo" @click="undo()">Undo</v-btn>
+          <v-btn :disabled="!canRedo" @click="redo()">Redo</v-btn>
+          <note>History (limited to 10 records for demo)</note>
+          {{ history }}
+          <div class="code-block mt-4">
+            <div v-for="i in history" :key="i.timestamp">
+              <span class="opacity-50 mr-2 font-mono">{{
+                format(i.timestamp)
+              }}</span>
+              <span class="font-mono">{ value: {{ i.snapshot }} }</span>
+            </div>
+            {{ stateData }}
+          </div>
+        </v-col>
+      </v-row>
+    </v-card>
+    <div v-if="isModalOpen" class="modal">
+      <v-card class="modal-content">
+        <p>ページを移動しますか？</p>
+        <v-btn @click="closeModal">キャンセル</v-btn>
+        <v-btn @click="confirmMove">移動する</v-btn>
+      </v-card>
+    </div>
+  </v-container>
 </template>
+
+<style scoped>
+.modal {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 999;
+}
+
+.modal-content {
+  width: 350px;
+  background-color: white;
+  padding: 20px;
+  border-radius: 5px;
+}
+</style>
